3cf755c26593ba7ff4289d1da325446a57f94c78
HongYingFang <zx04785@gmail.com>
2016-03-09 22:34:00 +0800

    Update and rename PrintCalendar to PrintCalendar.java
    divide the program to two class that include PrintCalendar.java and TestPrintCalendar.java

--- PrintCalendar.java
+++ PrintCalendar.java
@@ -1,38 +1,7 @@
 //U10416022
-import java.util.Calendar;
-//import java.util.GregorianCalendar;
 
 public class PrintCalendar {
-  /** Main method */
-  public static void main(String[] args) {
-	  //Data field
-	  Calendar day;
-	  
-	  int month1;
-	  int year1;
-	  //use Calendar method
-	  day = Calendar.getInstance();
-	  int userinput = args.length;
-	  //use if-else to give condition which it was input by user for the year,month or nothing
-	  //print out the calendar
-	  if (userinput == 2) {
-		  month1  = Integer.parseInt(args[0]);
-		  year1 = Integer.parseInt(args[1]);
-		  printMonth(year1,month1);
-	  }else if (userinput == 1) {
-		  month1  = Integer.parseInt(args[0]);
-		  year1 = (day.get(Calendar.YEAR));
-		  printMonth(year1,month1);
-	  } else {
-			  month1 = (day.get(Calendar.MONTH))+1;
-			  year1 = (day.get(Calendar.YEAR));
-			  printMonth(year1,month1);
-	  		}
-	  
-  }	  
-
-	  
-
+  
   /** Print the calendar for a month in a year */
   public static void printMonth(int year, int month) {
 	  // Print the headings of the calendar
